54133500419165666afd0596df4b7e9594774b10
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-02-11 14:14:04 +0530

    here we Go again

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Hero from "../components/sections/Hero";
+import {Hero} from "../components/sections/Hero";
 import Usage from "../components/sections/Usage";
 
 export default function Home() {
