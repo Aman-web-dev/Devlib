116bd4291cf22939a3c3aa6bb5f2904f302984c1
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-02-15 21:19:33 +0530

    Change

--- Components/Sidebar.jsx
+++ Components/Sidebar.jsx
@@ -10,6 +10,7 @@ function Sidebar(props) {
                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                 </svg>
+                
             </button>
 
             <div className='fixed  top-0 right-0 left-0 bottom-0 bg-gray-200 bg-opacity-50' onClick={() => props.handleScreenClick()} />
