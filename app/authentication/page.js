0ec5cb941e5c8e4f1cba524f450de26e23316085
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-02-17 19:58:17 +0530

    removed escape character

--- app/authentication/page.js
+++ app/authentication/page.js
@@ -168,7 +168,6 @@ function Page() {
               </form>
 
               <p className="mt-6 text-xs text-gray-600 text-center">
-                I agree to abide by templatana's
                 <a href="#" className="border-b border-gray-500 border-dotted">
                   Terms of Service
                 </a>
