5be416a0d2323a103418df6fe62362e4710d9bc1
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-03-09 17:22:01 +0530

    AFTER A LONG TIME

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,7 +8,7 @@ import { doSignOut } from "@/app/firebase/auth";
 import Image from "next/image";
 import Sidebar from "./Sidebar";
 
-const Navbar = () => {
+const Navbar = () => { 
 
   const defaultPhotoUrl="https://firebasestorage.googleapis.com/v0/b/devlib-c6572.appspot.com/o/assets%2Fpexels-sora-shimazaki-5668837.jpg?alt=media&token=6f17f0c5-dd45-4a69-a06b-28705ecd846e"
   const [navShow, setNavShow] = useState(false);
@@ -67,7 +67,7 @@ const Navbar = () => {
             onClick={() => {
               setNavShow(!navShow);
             }}
-            type="button"
+            type="button" 
             className="inline-flex items-center p-2 w-10 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 "
             aria-controls="navbar-default"
             aria-expanded="false"
@@ -118,10 +118,12 @@ const Navbar = () => {
                     <div className="flex flex-col md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                       {userLoggedIn ? (
                         <div className="flex flex-row">
+
                           <ProfileSection
                             ImgUrl={photoUrl}
                             signOut={doSignOut}
                           />
+
                         </div>
                       ) : (
                         <Link href="/authentication">
@@ -196,7 +198,7 @@ const ProfileSection = (props) => {
       <div>
         <img
           src={props.ImgUrl}
-          className="h-10 w-10 rounded-full cursor-pointer"
+          className="h-full w-[40%] rounded-full cursor-pointer"
           onClick={() => setOptionVisible(!optionsVisible)}
         />
         {optionsVisible ? (
