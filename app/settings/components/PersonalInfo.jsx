f71d8048cf896815b51bfd02894859c93640614d
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-03-13 21:21:38 +0530

    Changes

--- app/settings/components/PersonalInfo.jsx
+++ app/settings/components/PersonalInfo.jsx
@@ -5,7 +5,29 @@ function PersonalInfo() {
     <div className="min-h-screen bg-[#23272f] text-[#adb9c5] ">
    
 
+   <div className="w-[90%] m-auto">
+        <p className="text-2xl border-b py-4">Change Phone Number</p>
+        <p className="text-sm my-4">Change Your phone Number</p>
+        <button className="bg-[#373e47] py-2 px-2 rounded-lg my-2">
+          Change Phone Number
+        </button>
+      </div>
 
+      <div className="w-[90%]  m-auto">
+        <p className="text-2xl border-b py-4">Change Theme</p>
+        <p className="text-sm my-4">Chnage Your Color Scheme and</p>
+        <button className="bg-[#373e47] py-2 px-2 rounded-lg my-2">
+          Change Theme
+        </button>
+      </div>
+
+      <div className="w-[90%] m-auto">
+        <p className="text-2xl border-b py-4">Change Password</p>
+        <p className="text-sm my-4">Chnage Your login Password.</p>
+        <button className="bg-[#373e47] py-2 px-2 rounded-lg my-2">
+          Change username
+        </button>
+      </div>
 
 
 
--- app/settings/components/SecuritySetting.jsx
+++ app/settings/components/SecuritySetting.jsx
@@ -1,45 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function SecuritySetting() {
   return (
     <div className="min-h-screen bg-[#23272f] text-[#adb9c5] ">
-  
-   <div className="w-[90%] m-auto">
+      <div className="w-[90%] m-auto">
         <p className="text-2xl border-b py-4">Change Password</p>
-        <p className="text-sm my-4">
-          Chnage Your login Password.
-        </p>
+        <p className="text-sm my-4">Chnage Your login Password.</p>
         <button className="bg-[#373e47] py-2 px-2 rounded-lg my-2">
           Change username
         </button>
       </div>
-
 
       <div className="w-[90%]  m-auto">
         <p className="text-2xl border-b py-4">Change Password</p>
-        <p className="text-sm my-4">
-          Chnage Your login Password.
-        </p>
+        <p className="text-sm my-4">Chnage Your login Password.</p>
         <button className="bg-[#373e47] py-2 px-2 rounded-lg my-2">
           Change username
         </button>
       </div>
 
-
-      
-   <div className="w-[90%] m-auto">
+      <div className="w-[90%] m-auto">
         <p className="text-2xl border-b py-4">Change Password</p>
-        <p className="text-sm my-4">
-          Chnage Your login Password.
-        </p>
+        <p className="text-sm my-4">Chnage Your login Password.</p>
         <button className="bg-[#373e47] py-2 px-2 rounded-lg my-2">
           Change username
         </button>
       </div>
-
-
-  </div>
-  )
+    </div>
+  );
 }
 
-export default SecuritySetting
+export default SecuritySetting;
