b7941179bc464215c916f6df37707173352791dd
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-03-17 20:57:30 +0530

    Chnages

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,10 @@
-
 import { Inter } from "next/font/google";
 import React from "react";
 import "./globals.css";
-
 import Navbar from "./components/Navbar";
-
+import { ThemeProvider } from "next-themes";
 import Provider from "./provider";
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "Devlib",
   description: "A Library For Developers",
@@ -17,11 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <ThemeProvider enableSystem={true} attribute="class">
         <Provider>
           <Navbar />
           {children}
           <div id="portal"></div>
         </Provider>
+        </ThemeProvider>
       </body>
     </html>
   );
