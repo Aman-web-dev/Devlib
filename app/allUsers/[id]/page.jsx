79a854a727545d6d03ad9e1ecec9bcf662e2dbba
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-03-29 20:02:53 +0530

    user Search Functionality

--- app/allUsers/[id]/page.jsx
+++ app/allUsers/[id]/page.jsx
@@ -16,16 +16,12 @@ function page() {
 
   async function getUserData(userId) {
     try {
-        const response = await fetch("http://localhost:4000/get-userDeta", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ user_id: userId }),
-        });
-        console.log(response)
-        // const data
-        // console.log(data); // Logging the response data
+        const response = await axios.get("http://localhost:4000/get-userDeta",
+        {params:{
+          user_id:userId
+        }});
+        console.log(response.data[0]) 
+        setUserData(response.data[0])
     } catch (error) {
         console.log("error during fetching data: ", error);
     }
@@ -37,9 +33,13 @@ const user_id = "g6jvHvLep0aPXi32g4261t63y7P2" // Replace "your_user_id_here" wi
   return (
     <div>
       <button onClick={()=>getUserData(user_id)}>Click to fetch</button>
-      {/* {id} */}
+      {userData.user_id}
+     
     </div>
   )
 }
+
+
+
 
 export default page
