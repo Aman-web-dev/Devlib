43f95702c7ed5fff6abdb32f7a28ba3c39fe87ef
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-04-08 17:51:20 +0530

    Created user's api to Save authenticated user + creating user bucket

---
+++ app/authentication/customization/components/ProfileUpdater/personalDetailsUpdate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PersonalDetailsUpdate() {
+  return (
+    <div className='text-black'>
+      Here You can Update Your Perosnal Details Like user Name Name etc etc
+    </div>
+  )
+}
+
+export default PersonalDetailsUpdate
