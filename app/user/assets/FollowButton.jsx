0d999db6b77d628e31897a8d813353ec056567c2
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-04-16 23:38:26 +0530

    Added User follow, unfollow feature

--- app/user/assets/FollowButton.jsx
+++ app/user/assets/FollowButton.jsx
@@ -1,25 +1,36 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { followUser, checkUserFollowed } from "../api/api";
+import { followUser, checkUserFollowed, userUnfollow } from "../api/api";
 import { useAuth } from "@/utils (Context)/authContext";
 
 function FollowButton({ followed_id }) {
   const [followed, setFollowed] = useState(false);
   const { currentUser } = useAuth();
-
+  console.log("user Followed", followed_id);
+  const followedUserObj = {
+    follower_id: currentUser.uid,
+    followed_id: followed_id,
+  };
 
   const handleUserFollow = async () => {
-    const followedUserObj = {
-      follower_id: currentUser.uid,
-      followed_id: followed_id,
-    };
-    await followUser(followedUserObj, () => {});
+    await followUser(followedUserObj, (res) => {
+      console.log(res.message);
+      isUserFollowed();
+    });
+  };
+
+  const handleUserUnfollow = async () => {
+    await userUnfollow(followedUserObj, (res) => {
+      console.log(res.message);
+      isUserFollowed();
+    });
   };
 
   const isUserFollowed = async () => {
     try {
-      const res = await checkUserFollowed(followed_id);
+      const res = await checkUserFollowed(followedUserObj);
+      console.log("before follow Check", res);
       if (res == true) {
         setFollowed(true);
       } else {
@@ -30,20 +41,47 @@ function FollowButton({ followed_id }) {
     }
   };
 
+  if(currentUser.uid==followed_id){
+    return 
+  }
+
+
   useEffect(() => {
     isUserFollowed();
+  
   }, []);
 
   return (
-    <button
-      onClick={() => {
-        handleUserFollow;
-      }}
-      type="button"
-      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-    >
-      {followed?"Following":"Follow"}
-    </button>
+    
+      <div class="flex flex-wrap justify-center gap-6">
+        {followed ? (
+          <button
+            href="#"
+            class="relative"
+            onClick={() => {
+              handleUserUnfollow();
+            }}
+          >
+            <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-700"></span>
+            <span class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-black px-3 py-1 text-base font-bold text-white transition duration-100 hover:bg-gray-900 hover:text-yellow-500">
+              Following
+            </span>
+          </button>
+        ) : (
+          <button
+            class="relative"
+            href="#"
+            onClick={() => {
+              handleUserFollow();
+            }}
+          >
+            <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+            <span class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
+              Follow
+            </span>
+          </button>
+        )}
+      </div>
   );
 }
 
