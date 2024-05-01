b1efdf81a1bf9b107324e0a3cdb292786340f18b
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-05-01 22:58:45 +0530

    error solved

--- app/components/sections/Hero.jsx
+++ app/components/sections/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
   </div>
   <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
     <p className="text-sm leading-6 text-gray-900">
-      <strong className="font-semibold">Help Devlib</strong><svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Support Devlib's Programmers to keep Working on Devlib and make a better Programming Community.
+      <strong className="font-semibold">Help Devlib</strong><svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>Support Devlib&apos;s Programmers to keep Working on Devlib and make a better Programming Community.
     </p>
     <a href="#" className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900">Donate Now <span aria-hidden="true">&rarr;</span></a>
   </div>
