e2ff71fd224c8da57f8354ccf529c380d5103511
Rajnikantbhay <rsusishere@gmail.com>
2024-05-01 22:43:28 +0530

    implementing video page side bar

--- app/components/SearchComponent.jsx
+++ app/components/SearchComponent.jsx
@@ -43,7 +43,7 @@ const SearchComponent = ({
           />
         </div>
       )} */}
-      {videoSideBar && <VideoSideBar />}
+      {/* {videoSideBar && <VideoSideBar />} */}
       <DummySideBar
         Options={
           <Options
